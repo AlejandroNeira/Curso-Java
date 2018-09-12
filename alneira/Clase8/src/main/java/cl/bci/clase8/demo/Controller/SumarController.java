e8dba6c4ce50f31b6b7b6c8b9b9cef785e951ef7
AlejandroNeira <alejandro.neira@bci.cl>
2018-09-12 11:28:28 -0300

    Ejercicio sumar 3 numeros

---
+++ alneira/Clase8/src/main/java/cl/bci/clase8/demo/Controller/SumarController.java
@@ -0,0 +1,25 @@
+package cl.bci.clase8.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SumarController {
+
+    @GetMapping("sumar")
+    public String calcular(Model model){
+        model.addAttribute("n1",0);
+        model.addAttribute("n2",0);
+        model.addAttribute("n3",0);
+        return "Sumar";
+    }
+
+    @PostMapping("sumar")
+    public String plantilla(@RequestParam("n1") int n1, @RequestParam("n2") int n2, @RequestParam("n3") int n3, Model model){
+        model.addAttribute("suma",n1 + n2 + n3);
+        return "Sumar";
+    }
+}
