66277357ce4c72adc1b2213a1e226743dba90752
AlejandroNeira <alejandro.neira@bci.cl>
2018-09-12 11:33:19 -0300

    Mejora para que no refresque los numeros de la suma

--- alneira/Clase8/src/main/java/cl/bci/clase8/demo/Controller/SumarController.java
+++ alneira/Clase8/src/main/java/cl/bci/clase8/demo/Controller/SumarController.java
@@ -20,6 +20,10 @@ public class SumarController {
     @PostMapping("sumar")
     public String plantilla(@RequestParam("n1") int n1, @RequestParam("n2") int n2, @RequestParam("n3") int n3, Model model){
         model.addAttribute("suma",n1 + n2 + n3);
+        //para que no refresque los numeros
+        model.addAttribute("n1",n1);
+        model.addAttribute("n2",n2);
+        model.addAttribute("n3",n3);
         return "Sumar";
     }
 }
