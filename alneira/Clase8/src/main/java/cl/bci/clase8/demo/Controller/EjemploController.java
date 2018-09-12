34ef1575cf066efcbae7797e057b0b999438189d
AlejandroNeira <alejandro.neira@bci.cl>
2018-09-12 10:46:44 -0300

    Ejemplo de plantilla

--- alneira/Clase8/src/main/java/cl/bci/clase8/demo/Controller/EjemploController.java
+++ alneira/Clase8/src/main/java/cl/bci/clase8/demo/Controller/EjemploController.java
@@ -1,6 +1,7 @@
 package cl.bci.clase8.demo.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,6 +12,12 @@ public class EjemploController {
     @ResponseBody
     public String funcion(){
         return "hola mundo";
+    }
+
+    @GetMapping("ejemplo2")
+    public String plantilla(Model model){
+        model.addAttribute("valor","Hola Mundo :P");
+        return "plantilla";
     }
 
 }
