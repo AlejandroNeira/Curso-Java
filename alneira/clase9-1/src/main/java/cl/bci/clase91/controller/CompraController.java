4494ffb7970b2401893212fd98bb0607e320f752
AlejandroNeira <alejandro.neira@bci.cl>
2018-09-24 13:10:10 -0300

    Se agrega suma de la cantidad total

--- alneira/clase9-1/src/main/java/cl/bci/clase91/controller/CompraController.java
+++ alneira/clase9-1/src/main/java/cl/bci/clase91/controller/CompraController.java
@@ -2,6 +2,7 @@ package cl.bci.clase91.controller;
 
 import cl.bci.clase91.entity.Compra;
 import cl.bci.clase91.repository.CompraRepo;
+import cl.bci.clase91.services.CompraService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,9 +16,16 @@ public class CompraController {
     @Autowired
     private CompraRepo compraRepo;
 
+    @Autowired
+    private CompraService compraService;
+
     @GetMapping("/listarcompras")
     public String getMapping(Model modelo){
-        modelo.addAttribute("listaCompras", (List<Compra>)compraRepo.findAll());
+        List<Compra> lista = (List<Compra>)compraRepo.findAll();
+        modelo.addAttribute("listaCompras",lista );
+        modelo.addAttribute("total",compraService.calculoTotal(lista) );
         return "compras";
     }
+
+
 }
---
+++ alneira/clase9-1/src/main/java/cl/bci/clase91/services/CompraService.java
@@ -0,0 +1,21 @@
+package cl.bci.clase91.services;
+
+import cl.bci.clase91.entity.Compra;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompraService {
+
+    public int calculoTotal(List<Compra> lista){
+        int total = 0;
+        for (Compra compra: lista) {
+            total += (compra.getCantidad() * compra.getMontoUnitario());
+        }
+
+
+        return lista.stream().mapToInt(tc -> tc.getCantidad()* tc.getMontoUnitario()).sum();
+        //return total;
+    }
+}
