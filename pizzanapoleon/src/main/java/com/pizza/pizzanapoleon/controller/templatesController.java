3334de08a15430aba2216cf04c24074b7f351c5b
Maximiliano Ruiz <maximiliano.ruiz.g@bci.cl>
2018-10-23 11:39:05 -0300

    Pizzas mejorada

--- pizzanapoleon/src/main/java/com/pizza/pizzanapoleon/controller/templatesController.java
+++ pizzanapoleon/src/main/java/com/pizza/pizzanapoleon/controller/templatesController.java
@@ -10,9 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("pizzas")
 public class templatesController {
 
+    @GetMapping("/")
+    public String index(Model model){
+        return "index";
+    }
+
     @GetMapping("/login")
     public String login(Model model){
         return "login";
+    }
+
+    @GetMapping("/ordenar")
+    public String ordenar(Model model){
+        return "ordenar";
+    }
+
+    @GetMapping("/nosotros")
+    public String nosotros(Model model){
+        return "nosotros";
     }
 
 }
